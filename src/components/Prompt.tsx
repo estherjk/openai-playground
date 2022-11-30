acf632f2040c36d29077da2f9188214334b49ec0
Esther Jun Kim <hello@esthermakes.tech>
2022-11-30 14:31:30 -0800

    Update `Prompt` component w/ Textarea & Submit button

--- src/components/Prompt.tsx
+++ src/components/Prompt.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
-import { Box, Input, Text } from '@chakra-ui/react';
+import { Box, Button, Text, Textarea } from '@chakra-ui/react';
 
 import { requestText } from '@src/services';
 
 export const Prompt = () => {
   const [value, setValue] = useState('');
   const [result, setResult] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setValue(event.target.value);
   };
 
@@ -16,8 +17,12 @@ export const Prompt = () => {
     event.preventDefault();
 
     setResult('Processing...');
+    setIsLoading(true);
+
     const data = await requestText(value);
+
     setResult(data.result);
+    setIsLoading(false);
   };
 
   const renderResult = () => {
@@ -29,11 +34,14 @@ export const Prompt = () => {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <Input
+        <Textarea
           placeholder="Enter a few words to create a custom greeting."
           value={value}
           onChange={handleChange}
         />
+        <Button mt={4} colorScheme={'blue'} isLoading={isLoading} type="submit">
+          Submit
+        </Button>
       </form>
       <Box mt={4}>{renderResult()}</Box>
     </div>
